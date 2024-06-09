c63f01dda8d65b46e322583ca30e59010149d5c5
sometest66 <sometest66@gmail.com>
2024-06-09 10:22:21 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('i am the content inside index.js file4');
+const http = require('node:http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
